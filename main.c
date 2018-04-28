b8da16ab603172ea85c65d242428d7a8209d5a9b
Josh Cash <master_cash@live.com>
2018-04-27 20:27:39 -0500

    added a to-do list in the main file.

--- main.c
+++ main.c
@@ -12,7 +12,17 @@ void main()
 	uart_transmit('a');
 	uart_get();
 	//********************
-	
-	//stuff herex
+	/**********************
+		TO-DO
+		-------------------
+		Play two Short Tunes(~4 seconds long)
+		Display Song Title of Each on PC Term via serial
+		Make keyboard with atleast 3 buttons like keys
+		Use 1-2 buttons to control mode of opp(options above are modes)
+		must be able to change modes in middle of stored tune is playing
+		use 8051 ports to connect a secondary device via breadboard to do some opp
+		 - Idea: display of LEDs for different notes
+		Each Member had their own feature.
+	**********************/
 	
 }
