3b1c87fb8f8e4c2b4144abd7ede9c93f5ea1ca71
Josh Cash <master_cash@live.com>
2018-04-27 20:53:41 -0500

    added the look-table for the notes, hopefully they work.

--- main.c
+++ main.c
@@ -2,6 +2,28 @@
 #include <reg932.h>
 #include "uart.h"
 
+//current state of the project
+// mode = 0 - tune playing mode
+// mode = 1 - display songs
+// mode = 2 - keyboard mode
+char mode = 0;
+
+//Node Chart
+unsigned int notes[] = 
+{
+	0x06DE, // C
+	0x067F, // C#
+	0x061F, // D
+	0x05C9, // D#
+	0x0574, // E
+	0x0528, // F
+	0x04DD, // F#
+	0x0497, // G
+	0x0456, // G#
+	0x0417, // A
+	0x03DC, // A#
+	0x03A4, // B
+}
 
 void main()
 {
@@ -24,5 +46,17 @@ void main()
 		 - Idea: display of LEDs for different notes
 		Each Member had their own feature.
 	**********************/
-	
+	while(1)
+	{
+		switch(mode)
+		{
+			case 0:
+				break;
+			case 1:
+				break;
+			case 2:
+				break;
+		}
+	}
 }
+
