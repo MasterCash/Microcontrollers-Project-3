4bc51db968f0159692361ec2ea6107027e9f8017
Josh Cash <jjcfz2@mst.edu>
2018-05-04 15:22:24 -0500

    fixed frequency loads

--- main.c
+++ main.c
@@ -1,40 +1,41 @@
 //#include <reg51.h>
 #include <reg932.h>
-//#include "uart.h"
+#include "uart.h"
 
 //current state of the project
 // mode = 0 - tune playing mode
 // mode = 1 - display songs
 // mode = 2 - keyboard mode
-//char mode = 1;
-//sbit LED = P2^4;
+char mode = 0;
+sbit LED = P2^4;
 sbit SPEAKER = P1^7;
+
 
 //Note Chart C4 - B4
 unsigned int notes[] = 
 {
-	2348, // C
-	2216, // C#
-	2092, // D
-	1974, // D#
-	1863, // E
-	1759, // F
-	1660, // F#
-	1567, // G
-	1479, // G#
-	1396, // A
-	1318, // A#
-	1244, // B
+	14090, // C
+	13300, // C#
+	12553, // D
+	11848, // D#
+	11183, // E
+	10556, // F
+	9963, // F#
+	9404, // G
+	8876, // G#
+	8378, // A
+	7908, // A#
+	7464, // B
 };
 
 //1 = eighth note, 2 = quarter note, 3 = half note, 4 = whole note, 0 = articuation pause
 unsigned int beats[] =
 {
-  614,    // articuation pause !!Don't use this!! Internal only
-  5529,   // Eighth Note
-  11673,  // Quarter Note
-  17818,  // Half Note
-  23962   // Whole Note
+  163,    // articuation pause !!Don't use this!! Internal only
+  3932,   // Eighth Note
+  8028,  // Quarter Note
+  16220,  // Half Note
+  32605   // Whole Note
 };
 /*
 unsigned int beats2[] =
@@ -46,7 +47,7 @@ unsigned int beats2[] =
   12226
 };
 */
-unsigned int SEC = 6144 * 4;
+unsigned int SEC = 16384;
 
 
 
@@ -78,7 +79,7 @@ void time8b(unsigned char t)
 void timerover(unsigned int t)
 {
 	// looping over causes imprecise timing due to loop nature 
-	unsigned char loop = 100;
+	unsigned char loop = 225;
 	// make sure not to overide the Timer1 settings
 	// (hopefully this makes sure it doesn't mess up if it is running)
 	unsigned int currentTMOD = TMOD >> 4 << 4;
@@ -110,7 +111,7 @@ void timerover(unsigned int t)
 void playNote(unsigned int note, unsigned char type)
 {
 	// looping over causes imprecise timing due to loop nature 
-	unsigned char loop = 100;
+	unsigned char loop = 225;
 	unsigned int timerLoad = beats[type];
   unsigned int 
 	// set TMOD.
@@ -123,6 +124,7 @@ void playNote(unsigned int note, unsigned char type)
 		TL0 = -timerLoad;
 		TR0 = 1;
 		//run while timer goes, speaker may slightly change.
+    LED =0;
 		while(TF0 == 0)
 		{
       if(note > 0)
@@ -140,6 +142,7 @@ void playNote(unsigned int note, unsigned char type)
 		}
     TR0 = 0;
 		TF0 = 0;
+    LED =1;
 	}
   timerover(beats[0]); //articuation pause
 }
@@ -153,12 +156,12 @@ void playSong1()
 	timerover(SEC);
 		playNote(notes[4], 2); // quarter note
     playNote(notes[2], 2); // quarter note
-    playNote(notes[0], 3); 
-    playNote(0, 4); // example of a rest
+    playNote(notes[0], 3);
+    
     playNote(notes[4], 2);
     playNote(notes[2], 2);
     playNote(notes[0], 3);
-    playNote(0, 4);
+    
     playNote(notes[0], 1);
     playNote(notes[0], 1);
     playNote(notes[0], 1);
@@ -167,7 +170,7 @@ void playSong1()
     playNote(notes[2], 1);
     playNote(notes[2], 1);
     playNote(notes[2], 1);
-    playNote(0, 4);
+    
     playNote(notes[4], 2);
     playNote(notes[2], 2);
     playNote(notes[0], 3);
@@ -178,8 +181,8 @@ void playSong1()
 void main()
 {
 	P1M1 = 0x00;
+  P2M1 = 0x00;
 	EA = 1;
-  LED = 0;
 	//to remove warnings**
 	//uart_init();
 	//uart_transmit('a');
@@ -200,6 +203,20 @@ void main()
 	while(1)
 	{
     playSong1();
+    
+    switch(mode)
+    {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+    }
 	}
 }
 
