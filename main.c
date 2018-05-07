e0a24e88329ba98d251209488868492669beb0c7
Cash <master_cash@live.com>
2018-05-07 18:11:03 -0500

    changes

--- main.c
+++ main.c
@@ -6,15 +6,18 @@
 unsigned char mode = 0;
 sbit LED = P2^4;
 sbit SPEAKER = P1^7;
-sbit SA = P0^2;
-//sbit SB = P1^3;
-sbit SC = P3^1;
 sbit BTN1 = P2^0;
 sbit BTN2 = P0^1;
 sbit BTN3 = P2^3;
 
 
-unsigned int notes5[] = 
+sbit LED1 = P3^0;
+sbit LED2 = P3^1;
+
+code unsigned char HotxBuns[] = "Hot Cross Buns";
+code unsigned char Tetris[] = "Tetris Theme";
+
+code unsigned int notes4[] = 
 {
   7045,
   6649,
@@ -30,25 +33,25 @@ unsigned int notes5[] =
   3732
 };
 
-unsigned int notes6[] = 
+code unsigned int notes5[] = 
 {
-  3522,
-  3324,
-  3138,
-  2962,
-  2795,
-  2639,
-  2490,
-  2351,
-  2219,
-  2094,
-  1977,
-  1866
+  3522, //C 0
+  3324, //C# 1
+  3138, //D 2
+  2962, //D# 3
+  2795, //E 4
+  2639, //F 5
+  2490, //F# 6
+  2351, //G 7
+  2219, //G# 8
+  2094, //A 9
+  1977, //A# 10
+  1866 //B 11
 };
 
 
 //1 = eighth note, 2 = quarter note, 3 = half note, 4 = whole note, 0 = articuation pause bpm = 120
-unsigned int beats[] =
+code unsigned int beats[] =
 {
   163,    // articuation pause !!Don't use this!! Internal only
   3932,   // Eighth Note
@@ -63,6 +66,27 @@ unsigned int beats[] =
 };
 
 unsigned int SEC = 16384;
+
+void updateMode()
+{
+  LED1 = 0;
+  LED2 = 0;
+  switch(mode)
+  {
+    case 1: 
+      LED1 = 1;
+      break;
+    case 2:
+      LED2 = 1;
+      break;
+    case 3:
+      LED1 = 1;
+      LED2 = 1;
+      break;
+    case 4:
+      break;
+  }
+}
 
 /*
  * Expects Timer 0 to not be in use.
@@ -139,6 +163,12 @@ void playNote(unsigned int note, unsigned char type, bit tempo)
 }
 
 
+void song1Repeat()
+{
+  playNote(notes4[4], 2, 0); // quarter note
+  playNote(notes4[2], 2, 0); // quarter note
+  playNote(notes4[0], 3, 0);
+}
 
 /* 
  * Song number 1 Hot Cross Buns
@@ -147,97 +177,111 @@ void playSong1()
 {
 	//My attempt to make hot cross buns.
 	//timerover(SEC);
-  playNote(notes5[4], 2, 0); // quarter note
-  playNote(notes5[2], 2, 0); // quarter note
-  playNote(notes5[0], 3, 0);
   
-  playNote(notes5[4], 2, 0);
-  playNote(notes5[2], 2, 0);
-  playNote(notes5[0], 3, 0);
+  song1Repeat();
   
-  playNote(notes5[0], 1, 0);
-  playNote(notes5[0], 1, 0);
-  playNote(notes5[0], 1, 0);
-  playNote(notes5[0], 1, 0);
-  playNote(notes5[2], 1, 0);
-  playNote(notes5[2], 1, 0);
-  playNote(notes5[2], 1, 0);
-  playNote(notes5[2], 1, 0);
+  song1Repeat();
   
-  playNote(notes5[4], 2, 0);
-  playNote(notes5[2], 2, 0);
-  playNote(notes5[0], 3, 0);
+  playNote(notes4[0], 1, 0);
+  playNote(notes4[0], 1, 0);
+  playNote(notes4[0], 1, 0);
+  playNote(notes4[0], 1, 0);
+  playNote(notes4[2], 1, 0);
+  playNote(notes4[2], 1, 0);
+  playNote(notes4[2], 1, 0);
+  playNote(notes4[2], 1, 0);
+  
+  song1Repeat();
+  
   timerover(SEC);
 }
 
 void playSong2()
 {
-  playNote(notes6[4], 2, 1);
-  playNote(notes5[11], 1, 1);
-  playNote(notes6[0], 1, 1);
-  playNote(notes6[2], 2, 1);
-  playNote(notes6[0], 1, 1);
-  playNote(notes5[11], 1, 1);
+  playNote(notes5[4], 2, 1);
+  playNote(notes4[11], 1, 1);
+  playNote(notes5[0], 1, 1);
+  playNote(notes5[2], 2, 1);
+  playNote(notes5[0], 1, 1);
+  playNote(notes4[11], 1, 1);
 
-  playNote(notes5[9], 2, 1);
-  playNote(notes5[9], 1, 1);
-  playNote(notes6[0], 1, 1);
-  playNote(notes6[4], 2, 1);
-  playNote(notes6[2], 1, 1);
-  playNote(notes6[0], 1, 1);
+  playNote(notes4[9], 2, 1);
+  playNote(notes4[9], 1, 1);
+  playNote(notes5[0], 1, 1);
+  playNote(notes5[4], 2, 1);
+  playNote(notes5[2], 1, 1);
+  playNote(notes5[0], 1, 1);
 
-  playNote(notes5[11], 2, 1);
+  playNote(notes4[11], 2, 1);
   playNote(0, 1, 1);
-  playNote(notes6[0], 1, 1);
-  playNote(notes6[2], 2, 1);
-  playNote(notes6[4], 2, 1);
+  playNote(notes5[0], 1, 1);
+  playNote(notes5[2], 2, 1);
+  playNote(notes5[4], 2, 1);
 
-  playNote(notes6[0], 2, 1);
-  playNote(notes5[9], 2, 1);
-  playNote(notes5[9], 1, 1);
-  playNote(notes5[9], 1, 1);
-  playNote(notes5[11], 1, 1);
-  playNote(notes6[0], 1, 1);
+  playNote(notes5[0], 2, 1);
+  playNote(notes4[9], 2, 1);
+  playNote(notes4[9], 3, 1);
   timerover(SEC);
 }
 
 void keyboard()
 {
   if(BTN1 == 0)
-    holdNote(notes5[0]);
+    holdNote(notes4[0]);
   else if(BTN2 == 0)
-    holdNote(notes5[2]);
+    holdNote(notes4[2]);
   else if(BTN3 == 0)
-    holdNote(notes5[4]);
+    holdNote(notes4[4]);
+}
+
+void displaySong(unsigned char song[], unsigned char size)
+{
+  unsigned char k = 0;
+  for(;k < size; k++)
+  {
+   uart_transmit(song[k]); 
+  }
+  uart_transmit('\r');
+  uart_transmit('\n');       
 }
 
 void ExternInterrupt() interrupt 0 
 {
-    LED = 0;
-    //mode++;
+    mode++;
+    updateMode();
+    timerover(SEC);
+    timerover(SEC);
 }
+
+
 
 void main()
 {
-  unsigned char HotxBuns[] = "Hot cross buns";
+  
   unsigned char k = 0;
-	P1M1 = 0x00;
+	P1M1 = 0x04;
+  //P1M2 = 0x00;
+  //P2M2 = 0x00;
   P2M1 = 0x00;
+  //P0M2 = 0x00;
   P0M1 = 0x00;
+  P3M1 = 0x00;
   TMOD = 0x11;
-  IEN0 = 0x9F;  
-  mode = 2;
+  EX0 = 1;
+  mode = 0;
+  
 	EA = 1;
+  uart_init();
   
 	//to remove warnings**
-	//uart_transmit('a');
 	//uart_get();
+	//uart_transmit('a');
 	//********************
 	/**********************
 		TO-DO
 		-------------------
 		[X]Play two Short Tunes(~4 seconds long)
-		[]Display Song Title of Each on PC Term via serial
+		[X]Display Song Title of Each on PC Term via serial
 		[X]Make keyboard with atleast 3 buttons like keys
 		[]Use 1-2 buttons to control mode of opp(options above are modes)
       - must be able to change modes in middle of stored tune is playing
@@ -245,30 +289,38 @@ void main()
 		  -Using seven-segment display to show mode
       - Idea: display of LEDs for different notes4
 		[]Each Member had their own feature.
+    []
 	**********************/
-  //uart_init();
 	while(1)
 	{
-    SA  = 1;
-     
-    //playSong1();
-    playSong2();
+   
+    //updateMode LEDs and 7-segment Display
+    mode++;
+    updateMode();
     //controls what mode we do.
     switch(mode)
     {
       case 0:
-        keyboard();
-        //playSong2();
         break;
       case 1:
+        EX0 = 0;
+        displaySong(HotxBuns, 14);
+        EX0 = 1;
+        playSong1();
+       //mode++;
         break;
       case 2:
         break;
       case 3:
-
+        EX0 = 0;
+        displaySong(Tetris, 12);
+        playSong2();
+        //mode++;
         break;
       case 4:
-
+        break;
+      case 5:
+        keyboard();
         break;
     }
 	}
